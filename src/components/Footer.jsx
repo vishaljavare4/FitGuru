744c61a63609b0da0a12c12480639f3a05cc7135
vishaljavare4 GitHub <vishaljavare4@gmail.com>
2024-05-25 19:25:09 +0530

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -99,7 +99,7 @@ const Footer = () => {
     </section>
             <div className="h-[65px] w-[100%] bg-[rgb(47,48,50)] text-white flex items-center justify-between px-10">
                 <h1 className="font-bold text-xl text-center">FITGURU</h1>
-            <p className="font-bold text-xl text-center">Vishal Javare</p>
+            <p className="font-bold text-xl text-center">Anchit Julaniya</p>
             </div>
         </footer>    
     )
